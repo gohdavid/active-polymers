e77a73863c5dd744a7abee3459144632a35e0c1f
David <davidgoh@mit.edu>
2024-08-23 17:11:30 -0400

    Copy f_elas_loops and loops_with_srk1 for diff

--- act_pol/bdsim/forces.py
+++ act_pol/bdsim/forces.py
@@ -80,3 +80,23 @@ def f_conf_spring(x0, k_over_xi, Aex, rx, ry, rz):
             conf_u = conf_u / np.linalg.norm(conf_u)
             f[i] += Aex * conf_u * np.power(np.sqrt(conf) - 1, 3)
     return f
+
+@njit
+def f_elas_loops(x0, k_over_xi, relk, K, lamb=0):
+    """Compute spring forces on single, linear rouse polymer with additional
+    springs located at points in K matrix."""
+    N, _ = x0.shape
+    f = np.zeros(x0.shape)
+    for j in range(1, N):
+        f[j, :] += -lamb * x0[j, :]
+        for n in range(3):
+            f[j, n] += -k_over_xi*(x0[j, n] - x0[j-1, n])
+            f[j-1, n] += -k_over_xi*(x0[j-1, n] - x0[j, n])
+    # add additional springs at specific locations
+    for k in range(len(K)):
+        s1 = K[k][0]
+        s2 = K[k][1]
+        for n in range(3):
+            f[s1, n] += -relk * k_over_xi*(x0[s1, n] - x0[s2, n])
+            f[s2, n] += -relk * k_over_xi*(x0[s2, n] - x0[s1, n])
+    return f
